b28f1f21ade52055bdbfa1a0d148120fcebb69f4
Mykola Moskvin <nsmoskvin@gmail.com>
2019-08-29 09:18:36 +0300

    api directory added to the project. Serializer for restaurant added. RestaurantListView added.

--- test_task/test_restaurants/api/serializers.py
+++ test_task/test_restaurants/api/serializers.py
@@ -1,2 +1,9 @@
 from rest_framework import serializers
 
+from ..models import Restaurant, PreOrder, Reserved
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = '__all__'
---
+++ test_task/test_restaurants/api/views.py
@@ -0,0 +1,13 @@
+from ..models import Restaurant, PreOrder, Reserved
+from . import serializers
+from rest_framework import generics
+
+
+class RestaurantListView(generics.ListAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = serializers.RestaurantSerializer
+
+
+class RestaurantDetailView(generics.RetrieveAPIView):
+    queryset = Restaurant.objects.all()
+    serializer_class = serializers.RestaurantSerializer
